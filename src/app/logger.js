08c3dbaec61b283a7a1419ba330e26d06ef255b5
Eric Nishio <ericnishio@protonmail.com>
2017-10-19 21:18:25 +0300

    Simplify logger

--- src/app/logger.js
+++ src/app/logger.js
@@ -4,31 +4,11 @@ import winston from 'winston'
 
 import type {$Request, $Response, NextFunction} from 'express'
 
-type LogLevel = 'silly' | 'debug' | 'verbose' | 'info' | 'warn' | 'error'
-
-class Logger {
-  logger = new (winston.Logger)({
-    transports: [
-      new (winston.transports.Console)(),
-    ],
-  })
-
-  log = (level: LogLevel, message: string) => this.logger.log(level, message)
-
-  error = (message: string) => this.logger.error(message)
-
-  warn = (message: string) => this.logger.warn(message)
-
-  info = (message: string) => this.logger.info(message)
-
-  verbose = (message: string) => this.logger.verbose(message)
-
-  debug = (message: string) => this.logger.debug(message)
-
-  silly = (message: string) => this.logger.silly(message)
-}
-
-const logger = new Logger()
+const logger = new winston.Logger({
+  transports: [
+    new winston.transports.Console(),
+  ],
+})
 
 export const middleware = (req: $Request, res: $Response, next: NextFunction) => {
   res.on('finish', () => {
