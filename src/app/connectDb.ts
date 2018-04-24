46fa364540b81bb4229e07481707737f6966468d
Eric Nishio <ericnishio@protonmail.com>
2018-04-24 11:37:30 +0300

    Upgrade dependencies

--- src/app/connectDb.ts
+++ src/app/connectDb.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export default (): Promise<void> => new Promise((resolve, reject) => {
   mongoose.Promise = global.Promise
-  mongoose.connect(process.env.DB, {useMongoClient: true})
+  mongoose.connect(process.env.DB)
   mongoose.connection.on('open', resolve)
   mongoose.connection.on('error', reject)
 })
